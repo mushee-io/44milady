262760d143ee1f71c95e11294d9aa4e9e1b8ccd6
mushee <musheelabs@proton.me>
2026-09-23 07:40:54 +0100

    fix: box withdraw accounts to stay within Solana stack limit

--- programs/forty_four_milady/src/parts/accounts_collateral.rs
+++ programs/forty_four_milady/src/parts/accounts_collateral.rs
@@ -63,21 +63,21 @@ pub struct WithdrawCollateral<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
     #[account(seeds = [PROTOCOL_SEED], bump = protocol_config.bump)]
-    pub protocol_config: Account<'info, ProtocolConfig>,
+    pub protocol_config: Box<Account<'info, ProtocolConfig>>,
     #[account(
         mut,
         seeds = [CREDIT_SEED, owner.key().as_ref()],
         bump = credit_account.bump,
         constraint = credit_account.owner == owner.key() @ MiladyError::Unauthorized
     )]
-    pub credit_account: Account<'info, CreditAccount>,
+    pub credit_account: Box<Account<'info, CreditAccount>>,
     #[account(
         mut,
         seeds = [LENDING_POOL_SEED, lending_pool.usdg_mint.as_ref()],
         bump = lending_pool.bump,
         constraint = lending_pool.protocol == protocol_config.key() @ MiladyError::InvalidProtocol
     )]
-    pub lending_pool: Account<'info, LendingPool>,
+    pub lending_pool: Box<Account<'info, LendingPool>>,
     #[account(
         mut,
         seeds = [MARKET_SEED, collateral_mint.key().as_ref()],
@@ -85,15 +85,15 @@ pub struct WithdrawCollateral<'info> {
         constraint = market_config.protocol == protocol_config.key() @ MiladyError::InvalidProtocol,
         constraint = market_config.mint == collateral_mint.key() @ MiladyError::InvalidMint
     )]
-    pub market_config: Account<'info, MarketConfig>,
-    pub collateral_mint: InterfaceAccount<'info, Mint>,
+    pub market_config: Box<Account<'info, MarketConfig>>,
+    pub collateral_mint: Box<InterfaceAccount<'info, Mint>>,
     #[account(
         mut,
         token::mint = collateral_mint,
         token::authority = owner,
         token::token_program = token_program
     )]
-    pub owner_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub owner_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
     #[account(
         mut,
         seeds = [VAULT_SEED, credit_account.key().as_ref(), market_config.key().as_ref()],
@@ -102,7 +102,7 @@ pub struct WithdrawCollateral<'info> {
         token::authority = credit_account,
         token::token_program = token_program
     )]
-    pub collateral_vault: InterfaceAccount<'info, TokenAccount>,
+    pub collateral_vault: Box<InterfaceAccount<'info, TokenAccount>>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
